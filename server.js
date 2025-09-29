3f82e5b58d24a0d2d44618b5effe9950514dc295
bozylik <vaynberg.evgeniy@gmail.com>
2025-09-29 17:33:19 +0300

    Заменена локальная БД на API-запросы к серверу2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs').promises
 const path = require('path')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const DB_PATH = path.join(__dirname, 'events.json')
 const QUEUE_PATH = path.join(__dirname, 'queue.json')
 
